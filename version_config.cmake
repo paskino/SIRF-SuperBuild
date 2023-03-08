bb72512aa6d010ea887863838dd8c329667106b6
Edoardo Pasca <edo.paskino@gmail.com>
2023-03-08 13:59:49 +0000

    update to latest CIL

--- version_config.cmake
+++ version_config.cmake
@@ -156,7 +156,7 @@ set(DEFAULT_JSON_TAG v3.10.4)
 # CCPi CIL
 # minimum supported version of CIL supported is > 22.1.0 or from commit a6062410028c9872c5b355be40b96ed1497fed2a
 set(DEFAULT_CIL_URL https://github.com/TomographicImaging/CIL.git)
-set(DEFAULT_CIL_TAG a6062410028c9872c5b355be40b96ed1497fed2a)
+set(DEFAULT_CIL_TAG e3ccbc56788b59cae74ab59e30ae656dae18a07e)
 
 set(DEFAULT_CCPi-Regularisation-Toolkit_URL https://github.com/vais-ral/CCPi-Regularisation-Toolkit.git)
 set(DEFAULT_CCPi-Regularisation-Toolkit_TAG "v21.0.0")
